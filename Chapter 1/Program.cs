c80fa2c18b262cac79d9df650887a4ec7c7b8b8a
mandeeprana1 <ranamandeep093@gmail.com>
2026-09-23 16:36:56 +0530

    Var

--- Chapter 1/Program.cs
+++ Chapter 1/Program.cs
@@ -1,6 +1,8 @@
-﻿int a = 10;
-double b = 20.5;
-bool c = true;
-string d = "Hello, World!";
-Console.WriteLine(d);
-Console.WriteLine($"Integer: {a}, Double: {b}, Boolean: {c}");
+﻿var name = "John Doe";
+var age = 50;
+var salary = 75000.50;
+var isActive = true;
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(salary);
+Console.WriteLine(isActive);
