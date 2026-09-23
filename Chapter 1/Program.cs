cc3e4990819725453e960c221f07f5350ffe52ac
mandeeprana1 <ranamandeep093@gmail.com>
2026-09-23 19:28:15 +0530

    IF Conditions

--- Chapter 1/Program.cs
+++ Chapter 1/Program.cs
@@ -1,8 +1,22 @@
-﻿var name = "John Doe";
-var age = 50;
-var salary = 75000.50;
-var isActive = true;
-Console.WriteLine(name);
-Console.WriteLine(age);
-Console.WriteLine(salary);
-Console.WriteLine(isActive);
+﻿Console.WriteLine("Enter your Marks");
+int Marks = Convert.ToInt32(Console.ReadLine());
+ if(Marks > 90)
+{
+    Console.WriteLine("Grade A");
+}
+else if (Marks > 80)
+{
+    Console.WriteLine("Grade B");
+}
+else if (Marks > 70)
+{
+    Console.WriteLine("Grade C");
+}
+else if (Marks > 60)
+{
+    Console.WriteLine("Grade D");
+}
+else
+{
+    Console.WriteLine("False");
+}
