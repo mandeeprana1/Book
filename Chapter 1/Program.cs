6bdf7709f02dca417aba2f88b124145fecc4a272
mandeeprana1 <ranamandeep093@gmail.com>
2026-09-23 19:30:49 +0530

    Switch Statement

--- Chapter 1/Program.cs
+++ Chapter 1/Program.cs
@@ -1,22 +1,12 @@
-﻿Console.WriteLine("Enter your Marks");
-int Marks = Convert.ToInt32(Console.ReadLine());
- if(Marks > 90)
-{
-    Console.WriteLine("Grade A");
-}
-else if (Marks > 80)
-{
-    Console.WriteLine("Grade B");
-}
-else if (Marks > 70)
-{
-    Console.WriteLine("Grade C");
-}
-else if (Marks > 60)
-{
-    Console.WriteLine("Grade D");
-}
-else
-{
-    Console.WriteLine("False");
+﻿string Country= "USA";
+switch (Country){
+    case "USA":
+        Console.WriteLine("You are in the USA");
+        break;
+    case "Canada":
+        Console.WriteLine("You are in Canada");
+        break;
+    default:
+        Console.WriteLine("You are in another country");
+        break;
 }
