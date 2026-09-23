33a44d2ea471e753f628cdf01ca6d049470a5acc
mandeeprana1 <ranamandeep093@gmail.com>
2026-09-23 22:12:28 +0530

    Namespace Using

--- Chapter 1/Program.cs
+++ Chapter 1/Program.cs
@@ -1,41 +1,31 @@
-﻿enum Day
+﻿using System;
+
+namespace Company.HR
 {
-    Sunday,
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday
+    class Employee : IDisposable
+    {
+        public string Name { get; set; } = "";
+
+        public void Display()
+        {
+            Console.WriteLine("Employee Name: " + Name);
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
 }
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Enter a day:");
-        string input = Console.ReadLine();
-
-        Day today = Enum.Parse<Day>(input, true);
-
-        CheckDay(today);
-    }
-
-    static void CheckDay(Day day)
-    {
-        switch (day)
+        using (Company.HR.Employee emp = new Company.HR.Employee())
         {
-            case Day.Monday:
-            case Day.Tuesday:
-            case Day.Wednesday:
-            case Day.Thursday:
-            case Day.Friday:
-                Console.WriteLine("It's a weekday.");
-                break;
-
-            default:
-                Console.WriteLine("It's a weekend.");
-                break;
+            emp.Name = "Rahul";
+            emp.Display();
         }
     }
 }
