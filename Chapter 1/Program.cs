6c54fa99637a2b2b956fe547f947f63fd3d3953b
mandeeprana1 <ranamandeep093@gmail.com>
2026-09-23 15:36:28 +0530

    Interface

--- Chapter 1/Program.cs
+++ Chapter 1/Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿using System;
+
+ interface IPAy  {
+
+    public void Pay() { }
+
+}
+class CreditCard : IPAy
 {
-    private static void Main(string[] args)
+    public void Pay()
     {
-        Console.WriteLine("HELLO WORLD!");
+        Console.WriteLine("Paying with credit card");
     }
 }
+class PayPal : IPAy
+{
+    public void Pay()
+    {
+        Console.WriteLine("Paying with PayPal");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        IPAy p1=new PayPal();
+        p1.Pay();
+        IPAy p2=new CreditCard();
+        p2.Pay();
+
+    } }
