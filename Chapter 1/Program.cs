42b81a364d206034ebc223637e80dead6ff6a247
mandeeprana1 <ranamandeep093@gmail.com>
2026-09-23 20:09:44 +0530

    Enum

--- Chapter 1/Program.cs
+++ Chapter 1/Program.cs
@@ -1,6 +1,41 @@
-﻿for(int i=1; i <= 10; i++)
+﻿enum Day
 {
-    if (i == 5)
-        continue;
-    Console.WriteLine(i);
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a day:");
+        string input = Console.ReadLine();
+
+        Day today = Enum.Parse<Day>(input, true);
+
+        CheckDay(today);
+    }
+
+    static void CheckDay(Day day)
+    {
+        switch (day)
+        {
+            case Day.Monday:
+            case Day.Tuesday:
+            case Day.Wednesday:
+            case Day.Thursday:
+            case Day.Friday:
+                Console.WriteLine("It's a weekday.");
+                break;
+
+            default:
+                Console.WriteLine("It's a weekend.");
+                break;
+        }
+    }
 }
