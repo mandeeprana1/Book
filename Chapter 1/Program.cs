a63f9101736e0b4bc9b59dcf20c7cd5113ae6344
mandeeprana1 <ranamandeep093@gmail.com>
2026-09-23 19:36:57 +0530

    Multiple Case When

--- Chapter 1/Program.cs
+++ Chapter 1/Program.cs
@@ -1,12 +1,16 @@
-﻿string Country= "USA";
-switch (Country){
-    case "USA":
-        Console.WriteLine("You are in the USA");
+﻿int Number = 2;
+switch (Number)
+{
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        Console.WriteLine("Working DAY ");
         break;
-    case "Canada":
-        Console.WriteLine("You are in Canada");
+        case 5:
+        Console.WriteLine("The number is 5");
         break;
-    default:
-        Console.WriteLine("You are in another country");
+        default: Console.WriteLine("No Number Found");
         break;
+
 }
