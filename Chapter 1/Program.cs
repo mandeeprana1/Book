189475c04558d35c336e2d8b046cdf9e5481bc1a
mandeeprana1 <ranamandeep093@gmail.com>
2026-09-23 23:36:44 +0530

    MEthod OverLoading

--- Chapter 1/Program.cs
+++ Chapter 1/Program.cs
@@ -2,19 +2,28 @@
 
 class Program
 {
-    static void GetValues(out int x, out int y)
+   
+    static int Add(int a, int b)
     {
-        x = 10;
-        y = 20;
+        return a + b;
+    }
+
+   
+    static int Add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    
+    static double Add(double a, double b)
+    {
+        return a + b;
     }
 
     static void Main()
     {
-        int a, b;   
-
-        GetValues(out a, out b);
-
-        Console.WriteLine(a);
-        Console.WriteLine(b);
+        Console.WriteLine(Add(10, 20));
+        Console.WriteLine(Add(10, 20, 30));
+        Console.WriteLine(Add(10.5, 20.5));
     }
 }
