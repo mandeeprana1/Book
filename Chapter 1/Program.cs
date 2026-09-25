b2568014083a5e0a8ab78ebd373cd0c93497fdf2
mandeeprana1 <ranamandeep093@gmail.com>
2026-09-25 10:16:01 +0530

    3D Array

--- Chapter 1/Program.cs
+++ Chapter 1/Program.cs
@@ -2,19 +2,36 @@
 
 class Program
 {
-    static T GetDefault<T>()
-    {
-        return default(T);
-    }
-
     static void Main()
     {
-        int intValue = GetDefault<int>();
-        string stringValue = GetDefault<string>();
-        bool boolValue = GetDefault<bool>();
+        int[,] numbers =
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
 
-        Console.WriteLine("int: " + intValue);
-        Console.WriteLine("string: " + (stringValue ?? "null"));
-        Console.WriteLine("bool: " + boolValue);
+        int totalSum = 0;
+
+        Console.WriteLine("Array:");
+
+    
+        for (int i = 0; i < 3; i++)
+        {
+            int rowSum = 0;
+
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(numbers[i, j] + " ");
+
+                rowSum += numbers[i, j];
+                totalSum += numbers[i, j];
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Row " + (i + 1) + " Sum = " + rowSum);
+        }
+
+        Console.WriteLine("Total = " + totalSum);
     }
 }
