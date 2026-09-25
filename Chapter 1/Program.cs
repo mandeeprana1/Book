ebfbfe02a360e8efe3c3e789dd7664187a37390e
mandeeprana1 <ranamandeep093@gmail.com>
2026-09-25 10:31:54 +0530

    Delegate

--- Chapter 1/Program.cs
+++ Chapter 1/Program.cs
@@ -2,30 +2,27 @@
 
 class Program
 {
+  
+    delegate void Calculator(int a, int b);
+
+  
+    static void Add(int a, int b)
+    {
+        Console.WriteLine("Addition: " + (a + b));
+    }
+
+    static void Subtract(int a, int b)
+    {
+        Console.WriteLine("Subtraction: " + (a - b));
+    }
+
     static void Main()
     {
-        int number = 100;
-        Console.WriteLine("Original value: " + number);
+       
+        Calculator calc = Add;
+        calc(20, 10);
 
-    
-        object boxedValue = number;
-        Console.WriteLine("Boxed value: " + boxedValue);
-
-     
-        int unboxedValue = (int)boxedValue;
-        Console.WriteLine("Unboxed value: " + unboxedValue);
-
-     
-        object wrongValue = "Hello";
-
-        try
-        {
-            int wrongNumber = (int)wrongValue;
-            Console.WriteLine(wrongNumber);
-        }
-        catch (InvalidCastException)
-        {
-            Console.WriteLine("Wrong unboxing: Cannot convert string to int.");
-        }
+        calc = Subtract;
+        calc(20, 10);
     }
 }
